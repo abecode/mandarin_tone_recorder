5ab2a1034a8a3b61b861738c16dfe00ad08b6001
abecode <abe.kazemzadeh@gmail.com>
2026-07-14 17:29:53 -0500

    fixed issue with synchronizing pinyin: now there's an admin action to update pinyin if the hanzi is changed (need to select this in /admin/practice/practicedeck). also there is a conditional admin save that will update the pinyin if the pinyin was not edited but the hanzi was. also, if there is blank pinyin in a newly created practiceitem

--- src/mandarin_tone_recorder/practice/admin.py
+++ src/mandarin_tone_recorder/practice/admin.py
@@ -9,6 +9,7 @@ from mandarin_tone_recorder.practice.models import (
     PracticeItem,
     PracticeSession,
 )
+from mandarin_tone_recorder.practice.services import generate_pinyin_text
 
 
 class PracticeItemInline(admin.TabularInline):
@@ -35,6 +36,38 @@ class PracticeDeckAdmin(admin.ModelAdmin):
     @admin.display(description="Original pasted text")
     def original_pasted_text(self, deck: PracticeDeck) -> str:
         return deck.source_text
+
+
+@admin.register(PracticeItem)
+class PracticeItemAdmin(admin.ModelAdmin):
+    actions = ("regenerate_pinyin",)
+    list_display = ("prompt_text", "pinyin_text", "deck", "sort_order")
+    list_filter = ("deck",)
+    search_fields = ("prompt_text", "pinyin_text", "deck__title")
+
+    def save_model(self, request, obj, form, change) -> None:
+        if not change and not obj.pinyin_text:
+            obj.pinyin_text = generate_pinyin_text(obj.prompt_text)
+        elif (
+            change
+            and "prompt_text" in form.changed_data
+            and "pinyin_text" not in form.changed_data
+        ):
+            obj.pinyin_text = generate_pinyin_text(obj.prompt_text)
+        super().save_model(request, obj, form, change)
+
+    @admin.action(description="Regenerate pinyin for selected practice items")
+    def regenerate_pinyin(self, request, queryset) -> None:
+        updated_count = 0
+        for item in queryset:
+            item.pinyin_text = generate_pinyin_text(item.prompt_text)
+            item.save(update_fields=("pinyin_text",))
+            updated_count += 1
+
+        self.message_user(
+            request,
+            f"Regenerated pinyin for {updated_count} practice item(s).",
+        )
 
 
 admin.site.register(PracticeSession)
